97266ff9b01dcc31a150e650128beae21242dd8b
Pranav R <pranavkundapura06@gmail.com>
2025-04-04 22:15:38 +0530

    maintenance-page

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-links");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
