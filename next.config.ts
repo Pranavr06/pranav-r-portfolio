b9a9a19d768b77fade258e01b845e618dfdaf6a7
Pranav R <pranavkundapura06@gmail.com>
2026-06-07 15:53:18 +0530

    chore: add security headers to next.config.ts to improve security score

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval';"
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
