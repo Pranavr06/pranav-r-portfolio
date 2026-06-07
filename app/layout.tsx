d0b210ced814bba72ce59e155ba050afba51877a
Pranav R <pranavkundapura06@gmail.com>
2026-06-07 23:08:18 +0530

    feat: integrate Microsoft Clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,19 @@ export default function RootLayout({
           `,
           }}
         />
+        <Script
+          id="microsoft-clarity-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x3f2qdqm3r");
+            `,
+          }}
+        />
         <ToastProvider>
           <Suspense fallback={null}>
             <AnalyticsProvider />
